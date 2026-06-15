15e6c72caf60110056e8ada3c06d9e9f76dac721
ishx <pelarcaharveyischei@gmail.com>
2026-06-16 00:31:37 +0800

    Implemented input loop structure

--- BSIT31E3_PRELIM_A2_PELARCA_HARVEY/Program.cs
+++ BSIT31E3_PRELIM_A2_PELARCA_HARVEY/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿while (true)
+{
+    Console.Write("Enter first number (or 'exit' to quit): ");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+        break;
+}
